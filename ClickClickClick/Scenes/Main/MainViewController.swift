b3994634611e25dc25f2f01f7564bba026dae04d
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2022-12-03 22:25:16 +0300

    промежуточный коммит

--- ClickClickClick/Scenes/Main/MainViewController.swift
+++ ClickClickClick/Scenes/Main/MainViewController.swift
@@ -18,6 +18,27 @@ final class MainViewController: UIViewController {
     
     // MARK: - PrivateProperties
     
+    private let imageViewBackgroundScreen: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "mainBackground")
+        return imageView
+    }()
+    
+    private lazy var mainButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setBackgroundImage(
+            UIImage(named: "mainButton"),
+            for: .normal
+        )
+        button.addTarget(
+            self,
+            action: #selector(mainButtonPressed),
+            for: .touchUpInside
+        )
+        return button
+    }()
+    
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
@@ -26,7 +47,12 @@ final class MainViewController: UIViewController {
     }
     
     // MARK: - Actions
-
+    @objc
+    private func mainButtonPressed() {
+        mainButton.pushAnimate { [weak self] in
+            
+        }
+    }
 }
 
 // MARK: - MainViewProtocol Impl
@@ -37,14 +63,32 @@ extension MainViewController: MainViewProtocol {}
 
 private extension MainViewController {
     func setupViewController() {
-        view.backgroundColor = .systemGray
+        navigationController?.isNavigationBarHidden = true
         addSubViews()
         setupConstraints()
     }
     
-    func addSubViews() {}
+    func addSubViews() {
+        view.addSubviews(
+            imageViewBackgroundScreen,
+            mainButton
+        )
+    }
     
     func setupConstraints() {
-        NSLayoutConstraint.activate([])
+        let mainButtonHeight: CGFloat = 235
+        let mainButtonWidth: CGFloat = 250
+        
+        NSLayoutConstraint.activate([
+            imageViewBackgroundScreen.topAnchor.constraint(equalTo: view.topAnchor),
+            imageViewBackgroundScreen.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageViewBackgroundScreen.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            imageViewBackgroundScreen.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            mainButton.heightAnchor.constraint(equalToConstant: mainButtonHeight),
+            mainButton.widthAnchor.constraint(equalToConstant: mainButtonWidth),
+            mainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mainButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
